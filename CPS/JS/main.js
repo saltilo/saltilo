15de8172bcd92ad7c92a3be848eb141e9b0c17c8
saltilo <115881786+saltilo@users.noreply.github.com>
2024-06-09 13:20:21 +0300

    Add files via upload

--- CPS/JS/main.js
+++ CPS/JS/main.js
@@ -1,15 +1,47 @@
-document.getElementById("toggleBrands").addEventListener("click", function () {
-  const list = document.querySelector(".brands__list");
-  const button = document.getElementById("toggleBrands");
+// кнопка для текста
 
-  list.classList.toggle("shw-more");
+document.getElementById("toggleText").addEventListener("click", function () {
+  const textContainer = document.querySelector(".content__text");
+  const button = document.getElementById("toggleText");
+
+  textContainer.classList.toggle("content__text--show-more");
   button.classList.toggle("shown");
 
-  if (list.classList.contains("shw-more")) {
+  if (textContainer.classList.contains("content__text--show-more")) {
     button.innerHTML =
-      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Скрыть';
+      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Скрыть';
   } else {
     button.innerHTML =
-      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Показать всё';
+      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Показать всё';
   }
 });
+
+// кнопка для карточек
+document.getElementById("toggleBrands").addEventListener("click", function () {
+  const brandsContainer = document.querySelector(".brands__list");
+  const button = document.getElementById("toggleBrands");
+
+  brandsContainer.classList.toggle("shw-more");
+  button.classList.toggle("shown");
+
+  if (brandsContainer.classList.contains("shw-more")) {
+    button.innerHTML =
+      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Скрыть';
+  } else {
+    button.innerHTML =
+      '<img src="../Img/icons/arrow_down.svg" alt="Arrow"> Показать всё';
+  }
+});
+
+// бургер
+document.querySelector(".burger-btn").addEventListener("click", function () {
+  const menu = document.querySelector(".header__menu");
+  menu.classList.toggle("open");
+});
+
+document
+  .querySelector(".burger-btn-close")
+  .addEventListener("click", function () {
+    const menu = document.querySelector(".header__menu");
+    menu.classList.remove("open");
+  });
